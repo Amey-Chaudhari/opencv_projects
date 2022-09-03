443a8ba1f4997101e29d5818100988cf3c9ee0bb
Amey-Chaudhari <64803502+Amey-Chaudhari@users.noreply.github.com>
2022-09-03 19:16:59 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,8 +1,9 @@
+
 #importing the opencv module  
 import cv2  
   
 # using imread('path') and 0 denotes read as  grayscale image  
-img = cv2.imread(r'C:\Users\DEVANSH SHARMA\cat.jpeg',1)  
+img = cv2.imread(r'C:\Users\Lenovo\cat.jpeg',1)  
   
 #This is using for display the image  
 cv2.imshow('image',img)  
@@ -10,5 +11,5 @@ cv2.imshow('image',img)
 cv2.waitKey(3) # This is necessary to be required so that the image doesn't close immediately.  
 #It will run continuously until the key press.  
 cv2.destroyAllWindows()
-status = cv2.imwrite(r'C:\Users\DEVANSH SHARMA\cat.jpeg', 0, img)  
+status = cv2.imwrite(r'C:\Users\Lenovo\cat.jpeg', 0, img)  
 print("Image written to file-system : ", status) 
