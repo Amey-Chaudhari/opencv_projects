c228364a94ea4a42c0cef79cb6db541b25f5ba76
Amey-Chaudhari <64803502+Amey-Chaudhari@users.noreply.github.com>
2022-09-03 19:16:10 +0530

    Update Accessing_pixel.py

--- Accessing_pixel.py
+++ Accessing_pixel.py
@@ -1,5 +1,6 @@
+
 import numpy as np  
 import cv2  
-img = cv2.imread("C:\Users\DEVANSH SHARMA\cat.jpeg",1)  
+img = cv2.imread("C:\Users\Lenovo\cat.jpeg",1)  
 pixel = img[100,100]  
 print(pixel) 
